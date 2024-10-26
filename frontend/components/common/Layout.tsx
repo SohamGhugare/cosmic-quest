0a83b8ca7a7ccd9006dd00048c4029b8bea95e8b
Soham Ghugare <soham.ghugare@gmail.com>
2024-10-26 10:37:03 +0400

    feat: added frontend (landing page)

---
+++ frontend/components/common/Layout.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import { Container } from "@interchain-ui/react";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+
+export function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <Container maxWidth="64rem" attributes={{ py: "$14" }}>
+      <Head>
+        <title>Cosmic Quest</title>
+        <meta name="description" content="The ultimate web3 learning platform" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      {children}
+      <Footer />
+    </Container>
+  );
+}
